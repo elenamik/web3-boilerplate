d6c8863f27e38b3d14c554df7b57ea23a5e9251f
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-08-03 21:13:24 -0400

    react lint error

--- packages/ui/pages/index.tsx
+++ packages/ui/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useMoralis } from "react-moralis";
+import * as React from "react";
 
 const Home: NextPage = () => {
   const {
