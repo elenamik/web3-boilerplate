515b0af01fc75ec359391c6d710af53cdb6b59f0
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-08-15 16:25:18 -0400

    deploy broken

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,9 +1,18 @@
-require("@nomiclabs/hardhat-ethers")
-
 import { HardhatUserConfig } from "hardhat/config";
+
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false // defaults to false
+  },
 };
 
 export default config;
